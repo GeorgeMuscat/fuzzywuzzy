acbba3d50457ec676138fd37a7e5c993b9a11bf2
HamishWHC <20359560+HamishWHC@users.noreply.github.com>
2023-10-29 16:33:15 +1100

    updated installer

--- install.sh
+++ install.sh
@@ -8,13 +8,18 @@ sudo add-apt-repository -y ppa:deadsnakes/ppa
 
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get install -y python3.12 python3.12-dev libc6:i386 gcc-multilib g++-multilib git libssl-dev libffi-dev build-essential curl
+sudo apt-get install -y python3.12 python3.12-dev python3.12-venv libc6:i386 gcc-multilib g++-multilib git libssl-dev libffi-dev build-essential curl
 
 if [ "$(python3.12 -V | grep -vE 3\.12)" ]; then
     echo "Failed to install Python 3.12! Pls fix, thx melon."
     exit 1
 fi
 
+python3.12 -m venv .venv
+source .venv/bin/activate
+
 curl -sSL https://bootstrap.pypa.io/get-pip.py | python3.12 -
 python3.12 -m pip install -r requirements.txt
 python3.12 -m pip install -e .
+
+deactivate
